6fb9a253388623242978e6630f3bf7d5126bfb5a
aprlgpz <amsgapuz@gmail.com>
2024-09-04 16:30:59 +0800

    database changes

--- shopping-cart-app/src/app/dashboard/dashboard.component.ts
+++ shopping-cart-app/src/app/dashboard/dashboard.component.ts
@@ -17,3 +17,5 @@ export class DashboardComponent implements OnInit {
     });
   }
 }
+
+
--- shopping-cart-app/src/app/products/products.component.ts
+++ shopping-cart-app/src/app/products/products.component.ts
@@ -11,8 +11,6 @@ export class ProductsComponent  {
   products: any = [];
   constructor(private ProductService: ProductService) { }
 
-  //getProducts(): void {
-  //  this.products = this.ProductService.getProducts();}ngOnInit(): void { this.getProducts();}
   ngOnInit() {
   this.ProductService.getProducts().subscribe(data=>{
     this.products=data;
