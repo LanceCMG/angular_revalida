c83ee4b4bd5c317cf86ab5b891dc49d5afce2dc1
B0ssG33 <developer.geraldguevarra@gmail.com>
2024-09-11 16:21:38 +0800

    dashboard update

--- shopping-cart-app/src/app/app.module.ts
+++ shopping-cart-app/src/app/app.module.ts
@@ -18,6 +18,15 @@ import { AdminComponent } from './admin/admin.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AcknowledgmentComponent } from './acknowledgment/acknowledgment.component';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,9 +47,14 @@ import { AcknowledgmentComponent } from './acknowledgment/acknowledgment.compone
     FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatTabsModule,
+    MatButtonToggleModule,
+    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
